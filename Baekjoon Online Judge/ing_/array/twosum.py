89f21b4f00b1cf218a062c399540ec87cd364da9
nasa1515 <h43254@gmail.com>
2024-08-01 13:27:59 +0000

    readme update

--- Baekjoon Online Judge/ing_/array/twosum.py
+++ Baekjoon Online Judge/ing_/array/twosum.py
@@ -3,14 +3,14 @@
 check_list = [1, 2, 3, 4, 5]
 target = 4
 
-def target_search(check: list[int], targer) -> list[int]:
+def target_search(check: list[int], target) -> list[int]:
     # hash 검색 할 딕셔너리 생성
-    hash_dict = {}i
+    hash_dict = {}
     for i in range(len(check)):
         if target - check[i] not in hash_dict:
             hash_dict[check[i]] = i
         else:
-            return [check[hash_dict[target - check[]]], check[i]]
+            return [check[hash_dict[target - check[i]]], check[i]]
 
 
 print(target_search(check_list, target))
